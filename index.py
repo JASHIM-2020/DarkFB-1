831e9b7363597b431110b20be2b7ef589aa88dec
Cyber ID <52866599+Cyber-ID@users.noreply.github.com>
2019-12-19 19:26:21 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/use/bin/Python 2
 # Writer : Cyber ID
-# Date : Thursday, 19 December 2019
 # Time WIB : 19.24
+# Date : Thursday, 19 December 2019
 # Thank you to Shavanet Internet Stalls & 
 # The One God Who Gave Me a Programmer ...
 
